608fc45ea2e24a5a42bf7f95cf7d7b05b21fca20
Jeff Stuecher <stuecher.1@osu.edu>
2023-11-19 15:28:25 -0500

    Trying to add controller

---
+++ 5GuysCalcBlazor/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Web.Http;
+using _5GuysCalcBlazor;
+
+namespace FiveGuysCalcBlazor.API
+{
+    [System.Web.Http.Route("api/menu")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public int ReturnPlusOne(int input)
+        {
+            return input++;
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public int ReturnThree()
+        {
+            return 3;
+        }
+    }
+}
--- 5GuysCalcBlazor/Program.cs
+++ 5GuysCalcBlazor/Program.cs
@@ -1,11 +1,14 @@
 using _5GuysCalcBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Newtonsoft.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddControllers();
+
 
 await builder.Build().RunAsync();
