098638e05375d0902924b67e67ea77a03367b963
Jeff Stuecher <stue04@osumc.edu>
2023-12-01 20:35:15 -0500

    accordion and hyperlink selector added and working

--- 5GuysCalcBlazor/Program.cs
+++ 5GuysCalcBlazor/Program.cs
@@ -14,6 +14,7 @@ internal class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });;
         builder.Services.AddMvcCore();
+        builder.Services.AddBlazorBootstrap();
         //builder.Services.AddControllers();
         await builder.Build().RunAsync();
     }
