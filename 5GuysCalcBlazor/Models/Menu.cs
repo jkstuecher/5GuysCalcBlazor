a19b765d81cb1f706af9c2fff0d72c1d27b9e3e4
Jeff Stuecher <stuecher.1@osu.edu>
2023-12-03 10:25:43 -0500

    Checkbox work in progress

--- 5GuysCalcBlazor/Models/Menu.cs
+++ 5GuysCalcBlazor/Models/Menu.cs
@@ -7,9 +7,15 @@ namespace _5GCalc.Models
 {
     public class Menu
     {
-        public List<MenuItem> Items { get; set; }
+        public List<MenuItem>? Items { get; set; }
 
-        public List<string> Sections { get; set; }
+        public List<string>? Sections { get; set; }
+
+        public Menu() 
+        { 
+            Items = new List<MenuItem>();
+            Sections = new List<string>();
+        }
 
     }
 }
